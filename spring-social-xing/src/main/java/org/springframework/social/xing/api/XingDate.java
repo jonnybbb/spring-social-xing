edc32c6ae75fa1c798ceb7d8cad04e4a135b8795
Johannes Buehler <jonnybbb@gmail.com>
2014-08-14 18:31:21 +0200

    minor improvement

--- spring-social-xing/src/main/java/org/springframework/social/xing/api/XingDate.java
+++ spring-social-xing/src/main/java/org/springframework/social/xing/api/XingDate.java
@@ -1,13 +1,13 @@
 package org.springframework.social.xing.api;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class XingDate implements Serializable {
 
@@ -22,37 +22,37 @@ public class XingDate implements Serializable {
 
 	public XingDate(String dateString) {
 
-		if (dateString != null && !"".equals(dateString.trim())) {
+		if (dateString != null && !dateString.trim().isEmpty()) {
+            dateString = dateString.trim();
+            if (DEFAULT_DATE_FORMAT.length() == dateString.length()) {
+                try {
+                    SimpleDateFormat format = new SimpleDateFormat(
+                            DEFAULT_DATE_FORMAT);
+                    Calendar cal = Calendar.getInstance();
+                    Date date = format.parse(dateString);
+                    cal.setTime(date);
+                    month = cal.get(Calendar.MONTH) + 1; // MONTH is zero based
+                    year = cal.get(Calendar.YEAR);
+                } catch (ParseException e) {
+                    logger.fatal("failed to parse date string " + dateString, e);
+                }
+            } else if (FALLBACK_DATE_FORMAT.length() == dateString.length()) {
+                try {
+                    SimpleDateFormat fallbackFormat = new SimpleDateFormat(
+                            FALLBACK_DATE_FORMAT);
+                    Calendar cal = Calendar.getInstance();
+                    Date date = fallbackFormat.parse(dateString);
+                    cal.setTime(date);
+                    year = cal.get(Calendar.YEAR);
+                } catch (ParseException e) {
+                    logger.fatal("failed to parse date string " + dateString, e);
+                }
+            } else {
+                logger.fatal("unknown date format: " + dateString);
+            }
 
-			if (DEFAULT_DATE_FORMAT.length() == dateString.length()) {
-				try {
-					SimpleDateFormat format = new SimpleDateFormat(
-							DEFAULT_DATE_FORMAT);
-					Calendar cal = Calendar.getInstance();
-					Date date = format.parse(dateString);
-					cal.setTime(date);
-					month = cal.get(Calendar.MONTH) + 1; // MONTH is zero based
-					year = cal.get(Calendar.YEAR);
-				} catch (ParseException e) {
-					logger.fatal("failed to parse date string " + dateString, e);
-				}
-			} else if (FALLBACK_DATE_FORMAT.length() == dateString.length()) {
-				try {
-					SimpleDateFormat fallbackFormat = new SimpleDateFormat(
-							FALLBACK_DATE_FORMAT);
-					Calendar cal = Calendar.getInstance();
-					Date date = fallbackFormat.parse(dateString);
-					cal.setTime(date);
-					year = cal.get(Calendar.YEAR);
-				} catch (ParseException e) {
-					logger.fatal("failed to parse date string " + dateString, e);
-				}
-			} else {
-				logger.fatal("unknown date format: " + dateString);
-			}
-
-		}
-	}
+        }
+    }
 
 	public Integer getMonth() {
 		return month;
