db23e04153b5f5a4d8932a820e00a75bec4e1451
Thorsten Wendelmuth <thorsten.wendelmuth@amiando.com>
2015-04-24 16:08:28 +0200

    added new photo urls according to https://dev.xing.com/docs/user_profile

--- spring-social-xing/src/main/java/org/springframework/social/xing/api/PhotoUrls.java
+++ spring-social-xing/src/main/java/org/springframework/social/xing/api/PhotoUrls.java
@@ -1,56 +1,149 @@
 package org.springframework.social.xing.api;
 
-
 import java.io.Serializable;
 
-public class PhotoUrls implements Serializable{
+public class PhotoUrls implements Serializable {
 
-    private static final long serialVersionUID = 1379041303608063601L;
-    private String thumb;
-    private String miniThumb;
-    private String mediumThumb;
-    private String maxiThumb;
-    private String large;
+	private static final long serialVersionUID = 2190753534530022310L;
 
+	private String thumb;
 
-    public String getMediumThumb() {
-        return mediumThumb;
-    }
+	private String miniThumb;
 
-    public void setMediumThumb(String mediumThumb) {
-        this.mediumThumb = mediumThumb;
-    }
+	private String mediumThumb;
 
-    public String getThumb() {
-        return thumb;
-    }
+	private String maxiThumb;
 
-    public void setThumb(String thumb) {
-        this.thumb = thumb;
-    }
+	private String large;
 
-    public String getMiniThumb() {
-        return miniThumb;
-    }
+	private String size_32x32;
 
-    public void setMiniThumb(String miniThumb) {
-        this.miniThumb = miniThumb;
-    }
+	private String size_48x48;
 
-    public String getMaxiThumb() {
-        return maxiThumb;
-    }
+	private String size_64x64;
 
-    public void setMaxiThumb(String maxiThumb) {
-        this.maxiThumb = maxiThumb;
-    }
+	private String size_96x96;
 
-    public String getLarge() {
-        return large;
-    }
+	private String size_128x128;
 
-    public void setLarge(String large) {
-        this.large = large;
-    }
+	private String size_192x192;
+
+	private String size_256x256;
+
+	private String size_1024x1024;
+
+	private String size_Original;
+
+	public String getMediumThumb() {
+		return mediumThumb;
+	}
+
+	public void setMediumThumb(String mediumThumb) {
+		this.mediumThumb = mediumThumb;
+	}
+
+	public String getThumb() {
+		return thumb;
+	}
+
+	public void setThumb(String thumb) {
+		this.thumb = thumb;
+	}
+
+	public String getMiniThumb() {
+		return miniThumb;
+	}
+
+	public void setMiniThumb(String miniThumb) {
+		this.miniThumb = miniThumb;
+	}
+
+	public String getMaxiThumb() {
+		return maxiThumb;
+	}
+
+	public void setMaxiThumb(String maxiThumb) {
+		this.maxiThumb = maxiThumb;
+	}
+
+	public String getLarge() {
+		return large;
+	}
+
+	public void setLarge(String large) {
+		this.large = large;
+	}
+
+	public String getSize_32x32() {
+		return size_32x32;
+	}
+
+	public void setSize_32x32(String size_32x32) {
+		this.size_32x32 = size_32x32;
+	}
+
+	public String getSize_48x48() {
+		return size_48x48;
+	}
+
+	public void setSize_48x48(String size_48x48) {
+		this.size_48x48 = size_48x48;
+	}
+
+	public String getSize_64x64() {
+		return size_64x64;
+	}
+
+	public void setSize_64x64(String size_64x64) {
+		this.size_64x64 = size_64x64;
+	}
+
+	public String getSize_96x96() {
+		return size_96x96;
+	}
+
+	public void setSize_96x96(String size_96x96) {
+		this.size_96x96 = size_96x96;
+	}
+
+	public String getSize_128x128() {
+		return size_128x128;
+	}
+
+	public void setSize_128x128(String size_128x128) {
+		this.size_128x128 = size_128x128;
+	}
+
+	public String getSize_192x192() {
+		return size_192x192;
+	}
+
+	public void setSize_192x192(String size_192x192) {
+		this.size_192x192 = size_192x192;
+	}
+
+	public String getSize_256x256() {
+		return size_256x256;
+	}
+
+	public void setSize_256x256(String size_256x256) {
+		this.size_256x256 = size_256x256;
+	}
+
+	public String getSize_1024x1024() {
+		return size_1024x1024;
+	}
+
+	public void setSize_1024x1024(String size_1024x1024) {
+		this.size_1024x1024 = size_1024x1024;
+	}
+
+	public String getSize_Original() {
+		return size_Original;
+	}
+
+	public void setSize_Original(String size_Original) {
+		this.size_Original = size_Original;
+	}
 
 }
--- spring-social-xing/src/test/java/org/springframework/social/xing/api/impl/ProfileTemplateTest.java
+++ spring-social-xing/src/test/java/org/springframework/social/xing/api/impl/ProfileTemplateTest.java
@@ -41,73 +41,80 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
  */
 public class ProfileTemplateTest extends AbstractXingApiTest {
 
-    @Test
-    public void getUserProfile() {
-        mockServer.expect(requestTo(XingTemplate.DEFAULT_BASE_URL + ProfileTemplate.USERS_URL.replaceFirst("\\{id\\}", "me"))).andExpect(method(GET))
-                .andRespond(withSuccess(new ClassPathResource("testdata/profile.json", getClass()), MediaType.APPLICATION_JSON));
-        XingProfile userProfile = xing.profileOperations().getUserProfile();
-        assertEquals("6628146_33f97b", userProfile.getId());
-        assertEquals("Johannes", userProfile.getFirstName());
-        assertEquals("Bühler", userProfile.getLastName());
+	@Test
+	public void getUserProfile() {
+		mockServer.expect(requestTo(XingTemplate.DEFAULT_BASE_URL + ProfileTemplate.USERS_URL.replaceFirst("\\{id\\}", "me"))).andExpect(method(GET))
+				.andRespond(withSuccess(new ClassPathResource("testdata/profile.json", getClass()), MediaType.APPLICATION_JSON));
+		XingProfile userProfile = xing.profileOperations().getUserProfile();
+		assertEquals("6628146_33f97b", userProfile.getId());
+		assertEquals("Johannes", userProfile.getFirstName());
+		assertEquals("Bühler", userProfile.getLastName());
 
-        List<String> badges = userProfile.getBadges();
-        assertEquals(1, badges.size());
-        assertEquals("PREMIUM", badges.get(0));
+		List<String> badges = userProfile.getBadges();
+		assertEquals(1, badges.size());
+		assertEquals("PREMIUM", badges.get(0));
 
-        assertThat(userProfile.getPermalink(), is(not(nullValue())));
-        assertThat(userProfile.getProfessionalExperience(), is(not(nullValue())));
-        assertEquals(2, userProfile.getProfessionalExperience().getNonPrimaryCompanies().size());
-        assertEquals("CANOOENGINEERINGAG", userProfile.getProfessionalExperience().getPrimaryCompany().getTag());
-        assertEquals(Integer.valueOf(1), userProfile.getProfessionalExperience().getPrimaryCompany().getBeginDate().getMonth());
-        assertEquals("Senior Software Engineer", userProfile.getProfessionalExperience().getPrimaryCompany().getTitle());
-        assertEquals(CareerLevel.PROFESSIONAL_EXPERIENCED, userProfile.getProfessionalExperience().getPrimaryCompany().getCareerLevel());
-        assertThat(userProfile.getEducationalBackground(), is(not(nullValue())));
-        assertEquals(2, userProfile.getEducationalBackground().getSchools().size());
-        assertEquals(3, userProfile.getEducationalBackground().getQualifications().size());
-        assertEquals("Java Design Patterns Course Certificate", userProfile.getEducationalBackground().getQualifications().get(2));
+		assertThat(userProfile.getPermalink(), is(not(nullValue())));
+		assertThat(userProfile.getProfessionalExperience(), is(not(nullValue())));
+		assertEquals(2, userProfile.getProfessionalExperience().getNonPrimaryCompanies().size());
+		assertEquals("CANOOENGINEERINGAG", userProfile.getProfessionalExperience().getPrimaryCompany().getTag());
+		assertEquals(Integer.valueOf(1), userProfile.getProfessionalExperience().getPrimaryCompany().getBeginDate().getMonth());
+		assertEquals("Senior Software Engineer", userProfile.getProfessionalExperience().getPrimaryCompany().getTitle());
+		assertEquals(CareerLevel.PROFESSIONAL_EXPERIENCED, userProfile.getProfessionalExperience().getPrimaryCompany().getCareerLevel());
+		assertThat(userProfile.getEducationalBackground(), is(not(nullValue())));
+		assertEquals(2, userProfile.getEducationalBackground().getSchools().size());
+		assertEquals(3, userProfile.getEducationalBackground().getQualifications().size());
+		assertEquals("Java Design Patterns Course Certificate", userProfile.getEducationalBackground().getQualifications().get(2));
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.32x32.jpg", userProfile.getPhotoUrls().getSize_32x32());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.48x48.jpg", userProfile.getPhotoUrls().getSize_48x48());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.64x64.jpg", userProfile.getPhotoUrls().getSize_64x64());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.96x96.jpg", userProfile.getPhotoUrls().getSize_96x96());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.128x128.jpg", userProfile.getPhotoUrls().getSize_128x128());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.192x192.jpg", userProfile.getPhotoUrls().getSize_192x192());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.256x256.jpg", userProfile.getPhotoUrls().getSize_256x256());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.1024x1024.jpg", userProfile.getPhotoUrls().getSize_1024x1024());
+		assertEquals("http://www.xing.com/assets/frontend_minified/img/users/nobody_m.original.jpg", userProfile.getPhotoUrls().getSize_Original());
 
-    }
+	}
 
+	@Test
+	public void getFullUserProfile() {
+		mockServer.expect(requestTo(XingTemplate.DEFAULT_BASE_URL + ProfileTemplate.USERS_URL.replaceFirst("\\{id\\}", "me"))).andExpect(method(GET))
+				.andRespond(withSuccess(new ClassPathResource("testdata/full_profile.json", getClass()), MediaType.APPLICATION_JSON));
+		XingProfile userProfile = xing.profileOperations().getUserProfile();
+		assertEquals(4, userProfile.getLanguages().size());
+		assertEquals("ACM, GI", userProfile.getOrganisationMember());
+		assertEquals("m", userProfile.getGender());
 
-    @Test
-    public void getFullUserProfile() {
-        mockServer.expect(requestTo(XingTemplate.DEFAULT_BASE_URL + ProfileTemplate.USERS_URL.replaceFirst("\\{id\\}", "me"))).andExpect(method(GET))
-                .andRespond(withSuccess(new ClassPathResource("testdata/full_profile.json", getClass()), MediaType.APPLICATION_JSON));
-        XingProfile userProfile = xing.profileOperations().getUserProfile();
-        assertEquals(4, userProfile.getLanguages().size());
-        assertEquals("ACM, GI", userProfile.getOrganisationMember());
-        assertEquals("m", userProfile.getGender());
+		List<String> badges = userProfile.getBadges();
+		assertEquals(2, badges.size());
+		assertEquals("PREMIUM", badges.get(0));
+		assertEquals("MODERATOR", badges.get(1));
 
-        List<String> badges = userProfile.getBadges();
-        assertEquals(2, badges.size());
-        assertEquals("PREMIUM", badges.get(0));
-        assertEquals("MODERATOR", badges.get(1));
+		assertEquals("max.mustermann@xing.com", userProfile.getActiveEmail());
+		assertThat(userProfile.getEducationalBackground(), is(not(nullValue())));
+		assertEquals("1_abcdef", userProfile.getProfessionalExperience().getPrimaryCompany().getId());
+		assertEquals(Boolean.TRUE, userProfile.getProfessionalExperience().getPrimaryCompany().getUntilNow());
+		assertEquals(EmploymentStatus.EMPLOYEE, userProfile.getEmploymentStatus());
+		assertEquals(FormOfEmployment.FULL_TIME_EMPLOYEE, userProfile.getProfessionalExperience().getPrimaryCompany().getFormOfEmployment());
+		assertEquals("42_abcdef", userProfile.getEducationalBackground().getPrimarySchool().getId());
+		assertEquals(2, userProfile.getEducationalBackground().getQualifications().size());
+		assertEquals("PADI AOWD", userProfile.getEducationalBackground().getQualifications().get(1));
+		assertEquals(4, userProfile.getProfessionalExperience().getCompanies().size());
 
-        assertEquals("max.mustermann@xing.com", userProfile.getActiveEmail());
-        assertThat(userProfile.getEducationalBackground(), is(not(nullValue())));
-        assertEquals("1_abcdef", userProfile.getProfessionalExperience().getPrimaryCompany().getId());
-        assertEquals(Boolean.TRUE, userProfile.getProfessionalExperience().getPrimaryCompany().getUntilNow());
-        assertEquals(EmploymentStatus.EMPLOYEE, userProfile.getEmploymentStatus());
-        assertEquals(FormOfEmployment.FULL_TIME_EMPLOYEE, userProfile.getProfessionalExperience().getPrimaryCompany().getFormOfEmployment());
-        assertEquals("42_abcdef", userProfile.getEducationalBackground().getPrimarySchool().getId());
-        assertEquals(2, userProfile.getEducationalBackground().getQualifications().size());
-        assertEquals("PADI AOWD", userProfile.getEducationalBackground().getQualifications().get(1));
-        assertEquals(4, userProfile.getProfessionalExperience().getCompanies().size());
+	}
 
-    }
+	@Test
+	public void getProfileId() {
+		mockServer.expect(requestTo(XingTemplate.DEFAULT_BASE_URL + ProfileTemplate.USERS_URL.replaceFirst("\\{id\\}", "me"))).andExpect(method(GET))
+				.andRespond(withSuccess(new ClassPathResource("testdata/profile.json", getClass()), MediaType.APPLICATION_JSON));
+		assertEquals("6628146_33f97b", xing.profileOperations().getProfileId());
+	}
 
+	@Test
+	public void fullProfilFieldsAsString() throws Exception {
+		System.out.println(ProfileTemplate.FULL_PROFILE_FIELDS);
+		assertThat(ProfileTemplate.FULL_PROFILE_FIELDS.contains("first_name"), CoreMatchers.is(true));
 
-    @Test
-    public void getProfileId() {
-        mockServer.expect(requestTo(XingTemplate.DEFAULT_BASE_URL + ProfileTemplate.USERS_URL.replaceFirst("\\{id\\}", "me"))).andExpect(method(GET))
-                .andRespond(withSuccess(new ClassPathResource("testdata/profile.json", getClass()), MediaType.APPLICATION_JSON));
-        assertEquals("6628146_33f97b", xing.profileOperations().getProfileId());
-    }
-
-    @Test
-    public void fullProfilFieldsAsString() throws Exception {
-        System.out.println(ProfileTemplate.FULL_PROFILE_FIELDS);
-        assertThat(ProfileTemplate.FULL_PROFILE_FIELDS.contains("first_name"), CoreMatchers.is(true));
-
-    }
+	}
 }
